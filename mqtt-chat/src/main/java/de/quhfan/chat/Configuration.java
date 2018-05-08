db94d164f77e72576ed527d3c4d3701b9226d920
Kuhjunge <quhberta@gmail.com>
2018-05-08 13:08:01 +0200

    Gui speichert ingabe beim verbinden mit Server

--- mqtt-chat/src/main/java/de/quhfan/chat/Configuration.java
+++ mqtt-chat/src/main/java/de/quhfan/chat/Configuration.java
@@ -1,5 +1,3 @@
-package de.quhfan.chat;
-
 /*******************************************************************************
  * Copyright (C) 2017 Chris Deter
  *
@@ -21,11 +19,13 @@ package de.quhfan.chat;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  ******************************************************************************/
+package de.quhfan.chat;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -40,121 +40,163 @@ import java.util.logging.Logger;
  *
  */
 class Configuration {
+  private static final String CONF_LOC = "APPDATA";
+  private static final String EMPTYSTRING = "";
+  private static final String CONFIGFILEENDING = ".conf";
+  private static final String CONF_ERR = "Deleted faulty Config!";
+  private static final String ERROR_FOLDER = "Failed to create Folder!";
+  private static final String SEMICOLON = ";";
 
-	private static final Logger LOG = Logger.getLogger(Configuration.class.getName());
-	private File configPath = null;
-	private File configFile = null;
-	private final Map<String, String> data = new HashMap<>();
-	private String softwarename = "";
+  private static final Logger LOG = Logger.getLogger(Configuration.class.getName());
+  private File configPath = null;
+  private File configFile = null;
+  private final Map<String, String> data = new HashMap<>();
+  private String softwarename = "";
 
-	/**
-	 * Konstructor
-	 * 
-	 * @param softwarename
-	 * @param save
-	 */
-	public Configuration(final String softwarename, final Map<String, String> save) {
-		this.softwarename = softwarename;
-		configPath = new File(System.getenv("APPDATA") + File.separatorChar + softwarename);
-		data.putAll(save);
-	}
+  /**
+   * Konstruktor
+   *
+   * @param softwarename
+   * @param save
+   */
+  public Configuration(final String softwarename) {
+    this.softwarename = softwarename;
+    configPath = new File(System.getenv(Configuration.CONF_LOC) + File.separatorChar + softwarename);
+  }
 
-	/**
-	 * Hilfsfunktion, welche Arrays zu dem String "Element 1;Element 2;Element 3"
-	 * formatiert. Wird benötigt um das Array mit den Typen von Feldern zu
-	 * speichern.
-	 *
-	 * @param input
-	 *            ein Array mit Werten
-	 * @return kommagetrennte Array Elemente
-	 */
-	public String arrayToString(final String[] input) {
-		final StringBuilder builder = new StringBuilder();
-		for (final String value : input) {
-			builder.append(";" + value.trim());
-		}
-		builder.deleteCharAt(0);
-		return builder.toString();
-	}
+  /**
+   * gibt den Pfad des Ordners zurück in dem die Config gespeichert wird
+   *
+   * @return
+   */
+  public String getConfigPath() {
+    return configPath.getAbsolutePath();
+  }
 
-	public String getValue(final String key) {
-		return data.get(key);
-	}
+  /**
+   * Hilfsfunktion, welche Arrays zu dem String "Element 1;Element 2;Element 3"
+   * formatiert. Wird benötigt um das Array mit den Typen von Feldern zu
+   * speichern.
+   *
+   * @param input
+   *          ein Array mit Werten
+   * @return kommagetrennte Array Elemente
+   */
+  public static String arrayToString(final String[] input) {
+    final StringBuilder builder = new StringBuilder();
+    for (final String value : input) {
+      builder.append(Configuration.SEMICOLON + value.trim());
+    }
+    builder.deleteCharAt(0);
+    return builder.toString();
+  }
 
-	/**
-	 * initialisiert das Objekt und läd die Config von der Festplatte.
-	 *
-	 * @throws Exception
-	 *             Speichern fehlgeschlagen
-	 */
-	public void init() {
-		// ist der Pfad vorhanden
-		if (!configPath.exists() && !configPath.mkdirs()) {
-			Configuration.LOG.log(Level.SEVERE, "Ordner Fehlt");
-		}
-		// die Config Datei
-		configFile = new File(configPath.getAbsolutePath() + File.separatorChar + softwarename + ".conf");
-		// ist die Config Datei vorhanden - dann lade die daten
-		if (configFile.exists()) {
-			try (FileInputStream fis = new FileInputStream(configFile)) {
-				loadProperties(fis);
-			} catch (final IOException e) {
-				if (configFile.exists()) {
-					Configuration.LOG.log(Level.SEVERE, e.toString(), e);
-					// Lösche Config, wenn ein Fehler aufgetreten ist in der Config
-					if (configFile.delete()) {
-						Configuration.LOG.log(Level.WARNING, "Fehler!", e);
-					}
-				}
-			}
-		}
-	}
+  /**
+   * Wandelt einen String in einen Array um
+   *
+   * @param input
+   *          String
+   * @return Array
+   */
+  public static String[] stringToArray(final String input) {
+    final String temp = input.substring(1, input.length() - 2);
+    return Arrays.stream(temp.split(Configuration.SEMICOLON)).map(String::trim).toArray(String[]::new);
+  }
 
-	/**
-	 * Läd die Werte von der Festplatte.
-	 *
-	 * @param fis
-	 *            der File Input Stream
-	 * @throws IOException
-	 *             Wenn ein Fehler beim laden auftritt
-	 */
-	private void loadProperties(final FileInputStream fis) throws IOException {
-		final java.util.Properties prop = new java.util.Properties();
-		prop.load(fis);
-		for (final String k : data.keySet()) {
-			data.put(k, prop.getProperty(k, ""));
-		}
-	}
+  /**
+   * Läd einen Wert
+   *
+   * @param key
+   *          des Wertes
+   * @return der Wert
+   */
+  public String getValue(final String key) {
+    return data.get(key);
+  }
 
-	/**
-	 * Speichert die Config File auf die Festplatte.
-	 */
-	public void save() {
-		try (FileOutputStream fos = new FileOutputStream(configFile)) {
-			saveProperties(fos);
-		} catch (final IOException e) {
-			Configuration.LOG.log(Level.SEVERE, e.toString(), e);
-		}
-	}
+  /**
+   * initialisiert das Objekt und läd die Config von der Festplatte.
+   *
+   * @throws Exception
+   *           Speichern fehlgeschlagen
+   */
+  public void init() {
+    // ist der Pfad vorhanden
+    if (!configPath.exists() && !configPath.mkdirs()) {
+      Configuration.LOG.log(Level.SEVERE, Configuration.ERROR_FOLDER);
+    }
+    // die Config Datei
+    configFile = new File(
+        configPath.getAbsolutePath() + File.separatorChar + softwarename + Configuration.CONFIGFILEENDING);
+    // ist die Config Datei vorhanden - dann lade die daten
+    if (configFile.exists()) {
+      try (FileInputStream fis = new FileInputStream(configFile)) {
+        loadProperties(fis);
+      } catch (final IOException e) {
+        if (configFile.exists()) {
+          Configuration.LOG.log(Level.SEVERE, e.toString(), e);
+          // Lösche Config, wenn ein Fehler aufgetreten ist in der Config
+          if (configFile.delete()) {
+            Configuration.LOG.log(Level.WARNING, Configuration.CONF_ERR, e);
+          }
+        }
+      }
+    }
+  }
 
-	/**
-	 * Speichert die Werte auf der Festplatte.
-	 *
-	 * @param fos
-	 *            der File Output Stream
-	 * @throws IOException
-	 *             Wenn ein Fehler beim Speichern auftritt
-	 */
-	private void saveProperties(final FileOutputStream fos) throws IOException {
-		final java.util.Properties prop = new java.util.Properties();
-		for (final Entry<String, String> k : data.entrySet()) {
-			prop.put(k.getKey(), k.getValue());
-		}
-		prop.store(fos, softwarename);
-		fos.flush();
-	}
+  /**
+   * Läd die Werte von der Festplatte.
+   *
+   * @param fis
+   *          der File Input Stream
+   * @throws IOException
+   *           Wenn ein Fehler beim laden auftritt
+   */
+  private void loadProperties(final FileInputStream fis) throws IOException {
+    final java.util.Properties prop = new java.util.Properties();
+    prop.load(fis);
+    for (final String k : data.keySet()) {
+      data.put(k, prop.getProperty(k, Configuration.EMPTYSTRING));
+    }
+  }
 
-	public void setValue(final String key, final String value) {
-		data.put(key, value);
-	}
+  /**
+   * Speichert die Config File auf die Festplatte.
+   */
+  public void save() {
+    try (FileOutputStream fos = new FileOutputStream(configFile)) {
+      saveProperties(fos);
+    } catch (final IOException e) {
+      Configuration.LOG.log(Level.SEVERE, e.toString(), e);
+    }
+  }
+
+  /**
+   * Speichert die Werte auf der Festplatte.
+   *
+   * @param fos
+   *          der File Output Stream
+   * @throws IOException
+   *           Wenn ein Fehler beim Speichern auftritt
+   */
+  private void saveProperties(final FileOutputStream fos) throws IOException {
+    final java.util.Properties prop = new java.util.Properties();
+    for (final Entry<String, String> k : data.entrySet()) {
+      prop.put(k.getKey(), k.getValue());
+    }
+    prop.store(fos, softwarename);
+    fos.flush();
+  }
+
+  /**
+   * Setzt einen neuen Wert
+   *
+   * @param key
+   *          der Key
+   * @param value
+   *          der Wert
+   */
+  public void setValue(final String key, final String value) {
+    data.put(key, value);
+  }
 }
--- mqtt-chat/src/main/java/de/quhfan/chat/Controller.java
+++ mqtt-chat/src/main/java/de/quhfan/chat/Controller.java
@@ -3,8 +3,6 @@ package de.quhfan.chat;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 import de.quhfan.chat.mqtt.MQTTChat;
@@ -19,12 +17,8 @@ public class Controller implements PropertyChangeListener, Initializable {
 	private final static String user = "user";
 	private final static String pw = "pw";
 	private final static String certPath = "cert";
-	
+
 	private Configuration conf;
-	
-	public MQTTChat setUpChat(Configuration c) {
-		return new MQTTChat(c.getValue(user), c.getValue(pw), c.getValue(serverAddr), c.getValue(certPath));
-	}
 
 	Chat m;
 
@@ -33,26 +27,36 @@ public class Controller implements PropertyChangeListener, Initializable {
 
 	@FXML
 	private TextField textFieldMessage;
-	
+
 	@FXML
 	private TextField textBoxServer;
-	
+
 	@FXML
 	private TextField textBoxCertificate;
-	
+
 	@FXML
 	private TextField textFieldUser;
-	
+
 	@FXML
 	private TextField textFieldPw;
-	
+
 	@FXML
 	private TextField textFieldTopic;
 
 	public Controller() {
 	}
 
-	public void connect(String topic) {
+	@FXML
+	private void connect(final ActionEvent event) {
+		connect(textFieldTopic.getText());
+	}
+
+	public void connect(final String topic) {
+		conf.setValue(serverAddr, textBoxServer.getText());
+		conf.setValue(user, textFieldUser.getText());
+		conf.setValue(pw, textFieldPw.getText());
+		conf.setValue(certPath, textBoxCertificate.getText());
+		conf.save();
 		m = setUpChat(conf);
 		m.connect();
 		m.addPropertyChangeListener(this);
@@ -66,8 +70,7 @@ public class Controller implements PropertyChangeListener, Initializable {
 
 	@Override
 	public void initialize(final URL arg0, final ResourceBundle arg1) {
-		final Map<String, String> map = new HashMap<>();
-		conf = new Configuration("MQTTChat", map);
+		conf = new Configuration("MQTTChat");
 		conf.setValue(serverAddr, "tcp://127.0.0.1:1883");
 		conf.setValue(user, "admin");
 		conf.setValue(pw, "");
@@ -85,12 +88,7 @@ public class Controller implements PropertyChangeListener, Initializable {
 	public void propertyChange(final PropertyChangeEvent evt) {
 		setNews((String) evt.getNewValue());
 	}
-	
-	@FXML
-	private void connect(final ActionEvent event) {
-		connect(textFieldTopic.getText());
-	}
-	
+
 	@FXML
 	private void sendMessage(final ActionEvent event) {
 		sendMessage(m, textFieldMessage.getText());
@@ -103,5 +101,9 @@ public class Controller implements PropertyChangeListener, Initializable {
 
 	public void setNews(final String news) {
 		chatWindow.appendText(news);
+	}
+
+	public MQTTChat setUpChat(final Configuration c) {
+		return new MQTTChat(c.getValue(user), c.getValue(pw), c.getValue(serverAddr), c.getValue(certPath));
 	}
 }
